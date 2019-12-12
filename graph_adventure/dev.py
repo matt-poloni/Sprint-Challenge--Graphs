d86287da4d86aee65d4647bf2f466e67e04ccd76
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-12-12 14:26:27 -0500

    Initial commit

---
+++ graph_adventure/dev.py
@@ -0,0 +1,46 @@
+from room import Room
+from player import Player
+from world import World
+from adv import rg_3, rg_9, rg_12, rg_18, rg_500
+
+import random
+
+# Load world
+world = World()
+
+roomGraph = rg_500
+world.loadGraph(roomGraph)
+world.printRooms()
+player = Player("Matt Poloni", world.startingRoom)
+
+
+# FILL THIS IN
+traversalPath = []
+
+
+# TRAVERSAL TEST
+visited_rooms = set()
+player.currentRoom = world.startingRoom
+visited_rooms.add(player.currentRoom)
+for move in traversalPath:
+    player.travel(move)
+    visited_rooms.add(player.currentRoom)
+
+if len(visited_rooms) == len(roomGraph):
+    print(f"TESTS PASSED: {len(traversalPath)} moves, {len(visited_rooms)} rooms visited")
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f"{len(roomGraph) - len(visited_rooms)} unvisited rooms")
+
+
+
+#######
+# UNCOMMENT TO WALK AROUND
+#######
+# player.currentRoom.printRoomDescription(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     else:
+#         print("I did not understand that command.")
--- graph_adventure/world.py
+++ graph_adventure/world.py
@@ -12,10 +12,12 @@ class World:
         numRooms = len(roomGraph)
         rooms = [None] * numRooms
         gridSize = 1
-        for i in range(0, numRooms):
+        # Grab room IDs from graph for intermediate visuals
+        for i in roomGraph:
             x = roomGraph[i][0][0]
-            gridSize = max(gridSize, roomGraph[i][0][0], roomGraph[i][0][1])
-            self.rooms[i] = Room(f"Room {i}", f"({roomGraph[i][0][0]},{roomGraph[i][0][1]})",i, roomGraph[i][0][0], roomGraph[i][0][1])
+            y = roomGraph[i][0][1]
+            gridSize = max(gridSize, x, y)
+            self.rooms[i] = Room(f"Room {i}", f"({x},{y})",i, x, y)
         self.roomGrid = []
         gridSize += 1
         self.gridSize = gridSize
