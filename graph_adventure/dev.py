e54e2f0c3699f1562177ae6883c588879a38a1cc
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-12-12 14:41:43 -0500

    First pass at pruning function

--- graph_adventure/dev.py
+++ graph_adventure/dev.py
@@ -15,6 +15,49 @@ player = Player("Matt Poloni", world.startingRoom)
 
 
 # FILL THIS IN
+opposite = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+ones, twos, nodes, branches, cycles = {}, {}, {}, {}, {}
+visited = set()
+def prune(include=None):
+    # By default, consider the whole graph
+    if include is None:
+        include = roomGraph
+    # Initialize with all rooms that need to be covered
+    remaining = set(include.keys())
+    # Reset deadends (rooms w/ 1 link)
+    global ones; ones = {}
+    # Reset hallways (rooms w/ 2 links)
+    global twos; twos = {}
+    # Reset nodes (rooms w/ > 2 links)
+    global nodes; nodes = {}
+    # While there are still rooms to process
+    while len(remaining) > 0:
+        # Randomly select a remaining room
+        id = remaining.pop()
+        # Grab the (x, y) pair (for intermediate visuals)
+        xy = roomGraph[id][0]
+        # Grab all immediate connections in coverage area
+        links = {d: i for (d, i) in roomGraph[id][1].items() if i in include}
+        # Count number of relevant connections
+        num_links = len(links)
+        if num_links == 1:
+            # Mark as deadend
+            ones[id] = [xy, links]
+        elif num_links == 2:
+            # Mark as hallway
+            twos[id] = [xy, links]
+        elif num_links > 2:
+            # Mark as node
+            nodes[id] = [xy, links]
+prune()
+print('ONES', len(ones))
+print('TWOS', len(twos))
+print('NODES', len(nodes))
+while len(ones) > 0:
+    prune({**twos, **nodes})
+print('ONES', len(ones))
+print('TWOS', len(twos))
+print('NODES', len(nodes))
 traversalPath = []
 
 
